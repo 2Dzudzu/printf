023f8894f221d73e5e53c512402a5110eb3caf0e
2Dzudzu <petroszulu4@gmail.com>
2023-03-28 14:41:53 -0700

    exchange

--- write_handlers.c
+++ write_handlers.c
@@ -8,12 +8,12 @@
 * @width: get width.
 * @precision: precision specifier
 * @size: Size specifier
-**
-Return: Number of chars printed.
+* Return: Number of chars printed.
 */
 int handle_write_char(char c, char buffer[],
 int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
+{
+  /* char is stored at left and paddind at buffer's right */
 int i = 0;
 char padd = ' ';
 UNUSED(precision);
@@ -46,8 +46,7 @@ return (write(1, &buffer[0], 1));
 * @width: get width.
 * @precision: precision specifier
 * @size: Size specifier
-**
-Return: Number of chars printed.
+* Return: Number of chars printed.
 */
 int write_number(int is_negative, int ind, char buffer[],
 int flags, int width, int precision, int size)
@@ -76,8 +75,7 @@ length, padd, extra_ch));
 * @length: Number length
 * @padd: Pading char
 * @extra_c: Extra char
-**
-Return: Number of printed chars.
+* Return: Number of printed chars.
 */
 int write_num(int ind, char buffer[],
 int flags, int width, int prec,
@@ -133,11 +131,9 @@ return (write(1, &buffer[ind], length));
 * @width: Width specifier
 * @precision: Precision specifier
 * @size: Size specifier
-**
-Return: Number of written chars.
+* Return: Number of written chars.
 */
-int write_unsgnd(int is_negative, int ind,
-char buffer[],
+int write_unsgnd(int is_negative, int ind, char buffer[],
 int flags, int width, int precision, int size)
 {
 /* The number is stored at the bufer's right and starts at position i */
@@ -182,8 +178,7 @@ return (write(1, &buffer[ind], length));
 * @padd: Char representing the padding
 * @extra_c: Char representing extra char
 * @padd_start: Index at which padding should start
-**
-Return: Number of written chars.
+* Return: Number of written chars.
 */
 int write_pointer(char buffer[], int ind, int length,
 int width, int flags, char padd, char extra_c, int padd_start)
