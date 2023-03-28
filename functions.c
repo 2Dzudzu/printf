9c8d336d8d19094e7cc54b348a672ba8f0480aed
ThapeloPanda <ramathetsethapelo@gmail.com>
2023-03-28 13:28:53 -0700

    Updated

--- functions.c
+++ functions.c
@@ -11,7 +11,8 @@
 * @size: Size specifier
 * Return: Number of chars printed
 */
-int print_char(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 char c = va_arg(types, int);
 return (handle_write_char(c, buffer, flags, width, precision, size));
@@ -27,7 +28,8 @@ return (handle_write_char(c, buffer, flags, width, precision, size));
 * @size: Size specifier
 * Return: Number of chars printed
 */
-int print_string(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_string(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 int length = 0, i;
 char *str = va_arg(types, char *);
@@ -76,7 +78,8 @@ return (write(1, str, length));
 * @size: Size specifier
 * Return: Number of chars printed
 */
-int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_percent(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 UNUSED(types);
 UNUSED(buffer);
@@ -97,7 +100,8 @@ return (write(1, "%%", 1));
 * @size: Size specifier
 * Return: Number of chars printed
 */
-int print_int(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_int(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 int i = BUFF_SIZE - 2;
 int is_negative = 0;
@@ -133,7 +137,8 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
 * @size: Size specifier
 * Return: Numbers of char printed.
 */
-int print_binary(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 unsigned int n, m, i, sum;
 unsigned int a[32];
